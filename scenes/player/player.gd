85e1295d38b1684b3791e86df135d7a2e9850437
Pat Crane <67291819+PatCrane@users.noreply.github.com>
2025-11-09 21:35:10 -0500

    tool enums

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,9 +3,12 @@ extends CharacterBody2D
 @onready var move_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/MoveStateMachine/playback")
 
 var direction: Vector2
-var speed := 300
+var speed := 100
 
-func _physics_process(delta: float) -> void:
+enum Tools {HOE, AXE, WATER}
+var current_tool: Tools = Tools.AXE
+
+func _physics_process(_delta: float) -> void:
 	get_input()
 	velocity = direction * speed
 	move_and_slide()
@@ -14,6 +17,10 @@ func _physics_process(delta: float) -> void:
 func get_input():
 	direction = Input.get_vector("left", "right", "up", "down")
 	
+	if Input.is_action_just_pressed("tool_forward") or Input.is_action_just_pressed("tool_backward"):
+		var tool_diraction = Input.get_axis("tool_backward", "tool_forward") as int
+		current_tool = posmod(current_tool + tool_diraction, Tools.size()) as Tools
+		print(current_tool)
 
 func animation():
 	if direction:
