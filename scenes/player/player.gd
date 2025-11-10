572b8ec1e92b58f5e67887b85e30fa8c987783c4
Pat Crane <67291819+PatCrane@users.noreply.github.com>
2025-11-09 20:30:17 -0500

    player movement animations

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var move_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/MoveStateMachine/playback")
+
 var direction: Vector2
 var speed := 300
 
@@ -11,9 +13,14 @@ func _physics_process(delta: float) -> void:
 	
 func get_input():
 	direction = Input.get_vector("left", "right", "up", "down")
+	
 
 func animation():
 	if direction:
+		move_state_machine.travel('move')
 		var target_vector: Vector2 = Vector2(round(direction.x), round(direction.y))
-		$AnimationTree.set("parameters/MoveStateMachine/idle/blend_position", target_vector)	
+		$AnimationTree.set("parameters/MoveStateMachine/move/blend_position", target_vector)
+		$AnimationTree.set("parameters/MoveStateMachine/idle/blend_position", target_vector)
+	else:
+		move_state_machine.travel('idle')	
 	
