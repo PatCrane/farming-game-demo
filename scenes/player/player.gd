a0fd30a06b4cf8a9252b3580d72ad2d4b100171b
Pat Crane <67291819+PatCrane@users.noreply.github.com>
2025-11-09 19:30:43 -0500

    player animations

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -7,8 +7,13 @@ func _physics_process(delta: float) -> void:
 	get_input()
 	velocity = direction * speed
 	move_and_slide()
+	animation()
 	
 func get_input():
 	direction = Input.get_vector("left", "right", "up", "down")
-	
+
+func animation():
+	if direction:
+		var target_vector: Vector2 = Vector2(round(direction.x), round(direction.y))
+		$AnimationTree.set("parameters/MoveStateMachine/idle/blend_position", target_vector)	
 	
